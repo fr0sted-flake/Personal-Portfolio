ea86c5fbbfd93eb96cb426df0a98468231a6785b
Ayush Bhardwaj <ayush@lifeguru.app>
2023-12-20 17:17:21 +0530

    styled the title bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function NavBar() {
   return (
-    <FaBars/>
-  )
+    <div className="flex justify-between items-center w-full h-20 bg-black px-4 text-white">
+      <div>
+        <h1 className="text-5xl font-signature ml-2">Ayush</h1>
+      </div>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
