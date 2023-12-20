f57bd5174c4e2b1db58cb1590e514fc61fc4eb17
Ayush Bhardwaj <ayush@lifeguru.app>
2023-12-20 17:22:44 +0530

    style the home button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,6 +7,9 @@ function NavBar() {
       <div>
         <h1 className="text-5xl font-signature ml-2">Ayush</h1>
       </div>
+      <ul className="flex">
+        <li className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200">Home</li>
+      </ul>
     </div>
   );
 }
