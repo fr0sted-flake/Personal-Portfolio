8492e5e74a77854f561d193b94c53360f39a5fc4
Ayush Bhardwaj <ayush@lifeguru.app>
2023-12-28 18:54:39 +0530

    added the home bar tab

---
+++ src/components/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import HeroImage from "../assets/heroImage.png";
+import { MdKeyboardArrowRight } from "react-icons/md";
+
+const Home = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <h2>
+                    I am a Full Stack Developer
+                    </h2>
+                    <p>
+                        I have 1 year of experience building and designing software.
+                        Currently, I love to work on web application using technologies like
+                        React, Tailwind, Next JS and Flutter.
+                    </p>
+                    <div>
+                        <button>
+                            Portfolio
+                        </button>
+                    </div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
