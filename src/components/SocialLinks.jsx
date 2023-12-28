14b90f254acf654ab41c569514dec32533df501f
Ayush Bhardwaj <ayush@lifeguru.app>
2023-12-28 19:24:41 +0530

    imported some necessary icons

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 function SocialLinks() {
   return (
