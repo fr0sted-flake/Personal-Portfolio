3a4665f89296ce431332f04ec7a731675c765457
Ayush Bhardwaj <ayush@lifeguru.app>
2023-12-28 22:56:08 +0530

    added about section

---
+++ src/components/About.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <div>
+        <div>
+          <p>About</p>
+        </div>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta,
+          inventore? Fugit, temporibus commodi consequatur eveniet quia tenetur
+          aliquam dolorum sunt? Repudiandae voluptas dolor cum nulla dolorem
+          quo, facere, autem, iste cupiditate veniam culpa neque officiis natus
+          minima voluptatibus nesciunt rerum. Perferendis, fuga dolorum! Quidem
+          inventore sequi eos blanditiis quam minus!
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam fugiat
+          possimus odit ullam, eum repellendus eveniet tenetur repellat
+          accusamus dicta minus atque. Dolorem natus doloribus sed optio, modi
+          esse sequi? Illum, nobis est repellendus minima ipsum adipisci,
+          perspiciatis, ipsam labore dolore iste aliquid et totam porro at fugit
+          ut dolorem!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
