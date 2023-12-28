1f618e43498da7e8df583beda3c1938f4dff03de
Ayush Bhardwaj <ayush@lifeguru.app>
2023-12-28 23:10:33 +0530

    imported necessary assets

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar'
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
 import About from './components/About';
+import Portfolio from './components/Portfolio';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
      <NavBar/>
      <Home/>
      <About/>
+     <Portfolio/>
 
      <SocialLinks/>
     </>
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
+import installNode from "../assets/portfolio/installNode.jpg";
+import navbar from "../assets/portfolio/navbar.jpg";
+import reactParallax from "../assets/portfolio/reactParallax.jpg";
+import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
+import reactWeather from "../assets/portfolio/reactWeather.jpg";
+
+function Portfolio() {
+  return <div>Portfolio</div>;
+}
+
+export default Portfolio;
