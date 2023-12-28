77b8fa61cd54b52bbc25169159dcd8e50797588e
Ayush Bhardwaj <ayush@lifeguru.app>
2023-12-29 00:27:25 +0530

    styled the p tags

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,14 +2,19 @@ import React from "react";
 
 function Contact() {
   return (
-    <div>
-      <div>
-        <div>
-          <p>Contact</p>
-          <p>Submit the form below to get in touch with me</p>
+    <div
+      name="contact"
+      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
+    >
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Contact
+          </p>
+          <p className="py-6">Submit the form below to get in touch with me</p>
         </div>
         <div>
-          <form action="">
+          <form action="" className="">
             <input
               type="text"
               name="name"
