9ba6d1b0046416bed3033dd6779a34fb259fc5a4
Ayush Bhardwaj <ayush@lifeguru.app>
2023-12-28 22:52:10 +0530

    added responsiveness to the side bar

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -47,16 +47,21 @@ function SocialLinks() {
   ];
 
   return (
-    <div className="flex flex-col top-[35%] left-0 fixed">
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
       <ul>
-        <li className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:rounded-md duration-300 hover:ml-[-10px] bg-gray-500 ">
-          <a
-            href=""
-            className="flex justify-between items-center w-full text-white"
-          >
-            {""}
-          </a>
-        </li>
+        {links.map(({id, child, href, style, download}) => (
+          <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:rounded-md duration-300 hover:ml-[-10px] bg-gray-500 " + " "+style }>
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+              download={download}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
