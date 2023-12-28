f257740a96f70d24e12995f8f8b865cb94a10033
Ayush Bhardwaj <ayush@lifeguru.app>
2023-12-28 18:59:06 +0530

    added the styling in the main div of home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
+import React from "react";
 import HeroImage from "../assets/heroImage.png";
 import { MdKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div>
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black to-gray-800">
+      <div>
         <div>
-            <div>
-                <h2>
-                    I am a Full Stack Developer
-                    </h2>
-                    <p>
-                        I have 1 year of experience building and designing software.
-                        Currently, I love to work on web application using technologies like
-                        React, Tailwind, Next JS and Flutter.
-                    </p>
-                    <div>
-                        <button>
-                            Portfolio
-                        </button>
-                    </div>
-                
-            </div>
+          <h2>I am a Full Stack Developer</h2>
+          <p>
+            I have 1 year of experience building and designing software.
+            Currently, I love to work on web application using technologies like
+            React, Tailwind, Next JS and Flutter.
+          </p>
+          <div>
+            <button>
+              Portfolio
+              <span>
+                <MdKeyboardArrowRight />
+              </span>
+            </button>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
