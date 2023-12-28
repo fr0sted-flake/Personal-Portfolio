1da61f5d1ae843c9a079e52ff3720cdb47ba0fdf
Ayush Bhardwaj <ayush@lifeguru.app>
2023-12-28 19:05:48 +0530

    added the image and styled it

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,8 @@ import { MdKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div name="home" className="h-screen w-full bg-gradient-to-b from-black to-gray-800">
-      <div>
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black to-gray-800 text-white">
+      <div className="max-w-screen-lg  mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div>
           <h2>I am a Full Stack Developer</h2>
           <p>
@@ -21,6 +21,9 @@ const Home = () => {
               </span>
             </button>
           </div>
+        </div>
+        <div>
+            <img src={HeroImage} alt="My profile" className="rounded-2xl mx-auto w-2/3 md:w-full"/>
         </div>
       </div>
     </div>
