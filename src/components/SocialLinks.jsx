f883c07a845c098d64400d38d58ccc1015fde988
Ayush Bhardwaj <ayush@lifeguru.app>
2023-12-28 19:29:46 +0530

    styled the li tag

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -7,7 +7,7 @@ function SocialLinks() {
   return (
     <div className='flex flex-col top-[35%] left-0 fixed'>
         <ul>
-            <li>
+            <li className='flex justify-between items-center w-40 h-1/4 px-4 bg-white'>
                 <a href="">
                     {""}
                     <>
