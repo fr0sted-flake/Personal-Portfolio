6f3f8901d43bbef6d092818e6c1f2553b03971d1
Ayush Bhardwaj <ayush@lifeguru.app>
2023-12-29 00:14:36 +0530

    made the contact page

---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <div>
+        <div>
+          <p>Contact</p>
+          <p>Submit the form below to get in touch with me</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
