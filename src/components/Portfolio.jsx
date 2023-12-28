2c33b249e37b8fc7e3ac390882c89ef301a3ace5
Ayush Bhardwaj <ayush@lifeguru.app>
2023-12-28 23:29:20 +0530

    styled other elements

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -14,12 +14,12 @@ function Portfolio() {
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
           <p className="py-6">Check out some of my work right here</p>
         </div>
-        <div>
-        <div>
-          <img src={reactWeather} alt="" />
-          <div>
-            <button>Demo</button>
-            <button>Code</button>
+        <div className="grid sm:grid-cols-2 md: grid-cols-3 gap-8 px-12 sm:px-0">
+        <div className="shadow-md shadow-gray-600 rounded-lg">
+          <img src={reactWeather} alt="" className="rounded-md duration-200 hover:scale-105"/>
+          <div className="flex items-center justify-center">
+            <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
+            <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
           </div>
         </div>
         </div>
