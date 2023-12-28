1d59c20e6298f64106d1481f25eb305d701f3ba5
Ayush Bhardwaj <ayush@lifeguru.app>
2023-12-28 23:02:22 +0530

    styled the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 function About() {
   return (
-    <div>
-      <div>
-        <div>
-          <p>About</p>
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
         </div>
-        <p>
+        <p className="text-xl mt-20 ">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta,
           inventore? Fugit, temporibus commodi consequatur eveniet quia tenetur
           aliquam dolorum sunt? Repudiandae voluptas dolor cum nulla dolorem
@@ -16,7 +19,7 @@ function About() {
           inventore sequi eos blanditiis quam minus!
         </p>
         <br />
-        <p>
+        <p className="text-xl">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam fugiat
           possimus odit ullam, eum repellendus eveniet tenetur repellat
           accusamus dicta minus atque. Dolorem natus doloribus sed optio, modi
