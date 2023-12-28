291dbca68e6c59f9ef4ccc4aad18eeccac62ea7e
Ayush Bhardwaj <ayush@lifeguru.app>
2023-12-28 19:10:27 +0530

    styled the p and h2 tag

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,14 @@ import { MdKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div name="home" className="h-screen w-full bg-gradient-to-b from-black to-gray-800 text-white">
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black to-gray-800 "
+    >
       <div className="max-w-screen-lg  mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div>
-          <h2>I am a Full Stack Developer</h2>
-          <p>
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">I am a Full Stack Developer</h2>
+          <p className="text-gray-500 py-4 max-w-md">
             I have 1 year of experience building and designing software.
             Currently, I love to work on web application using technologies like
             React, Tailwind, Next JS and Flutter.
@@ -23,7 +26,11 @@ const Home = () => {
           </div>
         </div>
         <div>
-            <img src={HeroImage} alt="My profile" className="rounded-2xl mx-auto w-2/3 md:w-full"/>
+          <img
+            src={HeroImage}
+            alt="My profile"
+            className="rounded-2xl mx-auto w-2/3 md:w-full"
+          />
         </div>
       </div>
     </div>
